54034bc38fe8857ba5a1624146968020e73adc07
James Sherry <james.m.sherry@googlemail.com>
2023-04-10 14:38:54 +0100

    removed in-memory part

--- middleware.js
+++ middleware.js
@@ -2,23 +2,7 @@ const express = require('express');
 
 module.exports = function(app) {
   app.use(express.static('public'));
-  console.log('ji');
 
   app.use(express.json());
-
-  const cars = [];
-
-  app.get('/api/v1/cars',  (req, res) => {
-    res.status(200).json(cars);
-  });
-
-  app.get('/api/v1/drivers', (req, res) => {
-    res.status(200).json(cars);
-  });
-
-  app.post('/api/v1/cars', (req, res) => {
-    cars.push(req.body);
-    res.sendStatus(201);
-  })
 }
 
